459ed19eba75b1941a2179635e8230409ce493c5
musman-js <usman2037966@gmail.com>
2023-11-28 13:06:56 +0500

    learnign Javascript (ES6)

---
+++ let and const/app.js
@@ -0,0 +1,20 @@
+// ==== var ====
+var a = 'hello';
+var a = 'usman';
+a = 'ayaan'
+console.log(a)
+
+
+
+// ===== let ====
+let b = 'hello';
+// let b = 'values';    Because it is already define
+b = 'hello world';
+console.log(b)
+
+
+
+// ===== const =====
+const c = 'maths';
+// const c = 'computer'; because c is constant
+console.log(c)
