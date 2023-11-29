47b9b7a46f60f7b793c95b28fc8a62aee2102c15
Muhammad Usman <142093838+musman-js@users.noreply.github.com>
2023-11-29 12:02:58 +0500

    Add files via upload

---
+++ Template String/app.js
@@ -0,0 +1,18 @@
+// ====Normal====
+var name = " Muhammad Usman ";
+var greet = ' Hello' + name;
+console.log(greet)
+
+
+// =====ES6===
+// Template String
+
+let username = 'Muhammad Usman';
+let welcome = `Hello ${name} Welcome to Github `;
+console.log(welcome)
+
+
+const user1 = 'Muhammad Usman';
+const user2 = 'Web Development';
+const focus = `${user1} Focus on ${user2}`
+console.log(focus)
