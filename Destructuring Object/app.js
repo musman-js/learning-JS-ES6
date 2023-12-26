0247e929c215a617a0d375fd0ac651854dd8d4a2
Muhammad Usman <142093838+musman-js@users.noreply.github.com>
2023-12-26 22:28:43 +0500

    Destructuring Obj

---
+++ Destructuring Object/app.js
@@ -0,0 +1,23 @@
+let obj = {
+    name : 'usman' ,
+    age : 16 ,
+    field : 'Web Development'
+}
+
+let {name , age , field} = obj
+
+console.log(obj);
+console.log(age);
+console.log(field)
+
+
+
+
+let userObj = {
+    username : "ashar" , 
+    userage : "pata nh" 
+}
+
+let {username , userage}= userObj
+console.log(username)
+console.log(userage);
