bf72a728485ea435d825cfc23d0ecb9a286698c7
Muhammad Usman <142093838+musman-js@users.noreply.github.com>
2024-01-23 14:16:11 +0500

    Modules on progress

---
+++ Modules/app.js
@@ -0,0 +1,8 @@
+import {jname , age , username} from "./main.js"
+
+// console.log(jname)
+// console.log(age)
+
+let name  = "Ayaan"
+
+export{name}
---
+++ Modules/main.js
@@ -0,0 +1,16 @@
+import {name} from './app.js'
+
+let jname = "Muhammad Usman";
+let age = 16
+
+
+let userValue = () =>{
+
+}
+
+class username{
+    constructor(){
+    }
+}
+
+export{jname , age , username}
